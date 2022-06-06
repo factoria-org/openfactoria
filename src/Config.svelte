4bbcb439d9c54bb9260014af0b90133611821d0d
skogard <skogard@protonmail.com>
2022-06-06 02:51:32 -0400

    config save

--- src/Config.svelte
+++ src/Config.svelte
@@ -3,13 +3,13 @@ import localforage from 'https://esm.run/localforage';
 import Nav from './lib/Nav.svelte'
 let db = localforage.createInstance({ name: "api_key" })
 let key = "";
-let disabled = "";
+let disabled;
 db.getItem("key").then((_key) => {
   key = _key
 })
 const save = async () => {
   await db.setItem("key", key)
-  disabled = "disabled"
+  disabled = true
 }
 </script>
 <Nav config={"selected"} />
@@ -17,13 +17,19 @@ const save = async () => {
 <form class='form' on:submit|preventDefault|stopPropagation={save}>
   <div class='item'>
     <label>NFT.STORAGE API KEY</label>
-    <input type='text' placeholder="NFT.STORAGE key" bind:value={key}>
+    {#if disabled}
+      <input disabled type='text' placeholder="NFT.STORAGE key" bind:value={key}>
+    {:else}
+      <input type='text' placeholder="NFT.STORAGE key" bind:value={key}>
+    {/if}
   </div>
   <div class='item'>
-    {#if !disabled}
-    <input {disabled} type='submit' value="save" />
-    <br><br>
-    <div class='description'>You can get a free API key at <a href="https://nft.storage">https://nft.storage</a></div>
+    {#if disabled}
+      <div class='btn disabled'>saved</div>
+    {:else}
+      <input type='submit' value="save" />
+      <br><br>
+      <div class='description'>You can get a free API key at <a href="https://nft.storage">https://nft.storage</a></div>
     {/if}
   </div>
 </form>
@@ -41,5 +47,8 @@ const save = async () => {
   margin: 0;
   border: none;
   padding: 0;
+}
+.disabled {
+  background: gray;
 }
 </style>
